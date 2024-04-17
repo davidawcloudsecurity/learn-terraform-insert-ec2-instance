3498024e658a8967dddadafd5fc7641b81c2f0d6
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-17 19:39:54 +0800

    Update locals.tf windows & linux ami

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  ami  = "ami-0a699202e5027c10d"
+  ami_linux  = "ami-09b1e8fc6368b8a3a"
+ami_windows  = "ami-06c02c227f8655310"
   type = "t2.micro"
   tags = {
     Name = "My Virtual Machine"
