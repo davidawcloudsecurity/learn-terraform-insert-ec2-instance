08a615bd963be8e48ebe6d80a7c85d8f87f9cc36
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-21 11:32:26 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,19 @@ provider "aws" {
   region = var.aws_region
 }
 
+locals {
+  ami_linux    = "ami-0fe630eb857a6ec83"
+  ami_windows  = "ami-0d2a904b23cbe737a"
+  type_linux   = "t2.micro"
+  type_windows = "t2.micro"
+  key_pair_exists = can(data.aws_key_pair.existing[*].key_name)
+  tags = {
+    Project     = "IM8"
+    Name        = "Created by infra"
+    Environment = "Development" # Staging / Production / Development
+  }
+}
+
 // create new security group
 /*
 resource "aws_security_group" "example_server_sg" {
