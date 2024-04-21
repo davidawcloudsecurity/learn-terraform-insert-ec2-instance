31c7dd33a3718232e527a85d568ba40710db4863
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-21 11:31:27 +0800

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -3,6 +3,7 @@ locals {
   ami_windows  = "ami-0d2a904b23cbe737a"
   type_linux   = "t2.micro"
   type_windows = "t2.micro"
+  key_pair_exists = can(data.aws_key_pair.existing[*].key_name)
   tags = {
     Project     = "IM8"
     Name        = "Created by infra"
