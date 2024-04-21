dddeae12b99ba7e5809c8e445ccfffcb4ac9346c
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-21 11:30:58 +0800

    Update datas.tf

--- datas.tf
+++ datas.tf
@@ -2,3 +2,8 @@ data "aws_key_pair" "existing" {
   count   = local.key_pair_exists ? 1 : 0
   key_name = var.existing_key_pair
 }
+
+data "aws_key_pair" "generated" {
+  count   = local.key_pair_exists ? 0 : 1
+  key_name = var.existing_key_pair
+}
