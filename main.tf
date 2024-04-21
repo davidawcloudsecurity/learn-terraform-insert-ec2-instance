e55e2c1f5400f8d8d175883c59693de3495bcefc
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-21 11:39:04 +0800

    Update if key pair

--- main.tf
+++ main.tf
@@ -7,7 +7,8 @@ locals {
   ami_windows  = "ami-0d2a904b23cbe737a"
   type_linux   = "t2.micro"
   type_windows = "t2.micro"
-  key_pair_exists = can(data.aws_key_pair.existing[*].key_name)
+  // Check if the existing key pair exists
+  key_pair_exists = length(data.aws_key_pair.existing) > 0
   tags = {
     Project     = "IM8"
     Name        = "Created by infra"
