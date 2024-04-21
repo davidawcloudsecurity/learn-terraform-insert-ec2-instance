ac7b7fd90d5b7c6081d83043c8a52c5cccc98ad0
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-21 17:08:16 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -41,8 +41,7 @@ variable "new_key_pair_name" {
 
 variable "existing_key_pair" {
   description = "Name of the existing key pair"
-#  default     = "vpc-workshop"
-  default     = "vpc-worksho" //test error
+  default     = ""
 }
 
 variable "your_existing_vpc_id" {
