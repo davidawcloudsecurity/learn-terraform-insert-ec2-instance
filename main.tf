7173b96f444240ff7907925e6d94454a8d466add
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-04-21 11:32:47 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,16 @@ locals {
   }
 }
 
+data "aws_key_pair" "existing" {
+  count   = local.key_pair_exists ? 1 : 0
+  key_name = var.existing_key_pair
+}
+
+data "aws_key_pair" "generated" {
+  count   = local.key_pair_exists ? 0 : 1
+  key_name = var.existing_key_pair
+}
+
 // create new security group
 /*
 resource "aws_security_group" "example_server_sg" {
